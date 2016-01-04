1a6dcd21f0f7c1a6fbf0e86c36e82eea4a78919b
Sunny Sompalli <sunnysompalli@gmail.com>
2016-01-04 12:08:01 -0500

    Added hwSorts

---
+++ hwSorts/Driver.java
@@ -0,0 +1,10 @@
+public class Driver{
+
+    public static void main(String[]args){
+	int[]ary = { 8, 6, 7, 5, 3, 0, 9};
+	Sorts.printArray(ary);
+	Sorts.insertionSort(ary);
+	Sorts.printArray(ary);
+
+    }
+}
