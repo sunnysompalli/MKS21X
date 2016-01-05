e45a716db14a9b2a12f67b5a1ed84b1927689012
Sunny Sompalli <sunnysompalli@gmail.com>
2016-01-04 20:59:47 -0500

    made

---
+++ hwSorts/Sorts.java
@@ -0,0 +1,26 @@
+public class Sorts{
+    
+    public static void printArray(int[]data){
+	String ans = "[ ";
+	for (int i = 0; i < data.length - 1; i++){
+	    ans += (data[i] + ", ");
+	}
+	ans += data[data.length - 1];
+	ans += "]";
+	System.out.println(ans);
+    }
+    
+    public static void insertionSort(int[]data){
+	for (int i = 1; i < data.length; i++){
+	    int x = i - 1;
+	    int holder;
+	    while (x >= 0 && data[x + 1] < data[x]) {
+	        holder = data[x];
+	        data[x] = data[x + 1];
+		data[x + 1] = holder;
+		x -= 1;
+	    }
+	}
+    } 
+}
+
